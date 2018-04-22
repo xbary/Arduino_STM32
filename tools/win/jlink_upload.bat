aa8de8802a0102f6b1a3e1add17e7415d115f1a3
xbary <xbary@wp.pl>
2018-04-23 01:52:09 +0200

    zmieniony BAT aby za dużo sieki niepotrzebnej się nie wyświetlało.

--- tools/win/jlink_upload.bat
+++ tools/win/jlink_upload.bat
@@ -1,19 +1,17 @@
-rem: @echo off
-rem: Note %~dp0 get path of this batch file
-rem: Need to change drive if My Documents is on a drive other than C:
-set driverLetter=%~dp0
-set driverLetter=%driverLetter:~0,2%
-%driverLetter%
-cd %~dp0
-rem: the two line below are needed to fix path issues with incorrect slashes before the bin file name
-set tmpBinFilePath=%1
-set tmpBinFilePath=%tmpBinFilePath:/=\%
-
-rem: create commander script file with the tmp bin that the Arduino IDE creates
-
+@echo off
+@rem: Note %~dp0 get path of this batch file
+@rem: Need to change drive if My Documents is on a drive other than C:
+@set driverLetter=%~dp0
+@set driverLetter=%driverLetter:~0,2%
+@%driverLetter%
+@cd %~dp0
+@rem: the two line below are needed to fix path issues with incorrect slashes before the bin file name
+@set tmpBinFilePath=%1
+@set tmpBinFilePath=%tmpBinFilePath:/=\%
+@rem: create commander script file with the tmp bin that the Arduino IDE creates
 @echo erase > %tmpbinfilepath%.jlink
 @echo loadbin %tmpbinfilepath% , 0x8000000 >> %tmpbinfilepath%.jlink
 @echo r >> %tmpbinfilepath%.jlink
 @echo q >> %tmpbinfilepath%.jlink
 
-"C:\Program Files (x86)\SEGGER\JLink_V614b\JLink.exe" -device %2 -if SWD -speed auto -CommanderScript %tmpBinFilePath%.jlink
+"C:\Program Files (x86)\SEGGER\JLink_V614b\JLink.exe" -device %2 -if SWD -speed auto -CommanderScript %tmpBinFilePath%.jlink > nul
